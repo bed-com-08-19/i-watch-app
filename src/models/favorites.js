0fb01fbae52c77df8c17bcb62d8bdbe5a2c9f9be
prow57naff <95228455+prow57@users.noreply.github.com>
2024-05-13 04:59:54 +0200

    updated models

--- src/models/favorites.js
+++ src/models/favorites.js
@@ -1,18 +1,31 @@
-import mongoose from "mongoose";
+import connectToDatabase from '../../lib/db';
+import User from "./User";
 
-const NewFavoriteSchema = new mongoose.Schema(
-  {
-    uid: String,
-    accountID: String,
-    backdrop_path: String,
-    poster_path: String,
-    movieID: Number,
-    type: String,
-  },
-  { timestamps: true }
-);
-
-const Favorites =
-  mongoose.models.Favorites || mongoose.model("Favorites", NewFavoriteSchema);
-
-export default Favorites;
+export default async function handler(req, res) {
+    const { method, userId, videoId } = req.body;
+  
+    if (method === 'POST') {
+      try {
+        await connectToDatabase();
+        const user = await User.findById(userId);
+        user.favorites.push(videoId);
+        await user.save();
+        res.status(200).json({ success: true, message: 'Video added to favorites' });
+      } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+      }
+    } else if (method === 'DELETE') {
+      try {
+        await connectToDatabase();
+        const user = await User.findById(userId);
+        user.favorites = user.favorites.filter(id => id !== videoId);
+        await user.save();
+        res.status(200).json({ success: true, message: 'Video removed from favorites' });
+      } catch (error) {
+        res.status(500).json({ success: false, error: error.message });
+      }
+    } else {
+      res.status(405).json({ success: false, message: 'Method Not Allowed' });
+    }
+  }
+  
