76a9dbe819abd66fe3ed7ec6264a9af4ebde9b31
prow57naff <95228455+prow57@users.noreply.github.com>
2024-05-15 13:31:53 +0200

    Fixed the login issue

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const userSchema = new mongoose.Schema({
-   username: {
-   type: String,
-   required: [true, "Please provide a username"],
-   },
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+  },
   email: {
     type: String,
     required: [true, "Please provide an email"],
@@ -26,13 +26,21 @@ const userSchema = new mongoose.Schema({
     default: false,
   },
   favoriteVideos: [{ type: Schema.Types.ObjectId, ref: 'Video' }],
-  img: {type: String, default: 0},
+  img: {
+    type: String,
+    default: "",
+  },
   preferredCategories: [{ type: String }],
-  balance: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-  
+  balance: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.models.users || mongoose.model("user", userSchema);
+const UserModel = mongoose.models.User || mongoose.model("User", userSchema);
 
-module.exports = User;
+module.exports = UserModel;
