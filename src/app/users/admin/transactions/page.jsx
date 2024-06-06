3f9bdc0d73e62ad3c07c440afcc0733bd581dcd3
prow57naff <bed-com-08-19@unima.ac.mw>
2024-06-06 16:01:17 +0200

    Create page.jsx

---
+++ src/app/users/admin/transactions/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+import Sidebar from '../../../../components/Sidebar';
+import Navbar from '../../../../components/Navbar';
+import EarningsReport from '../../../../components/AdminTools/EarningsReport';
+
+const Transactions = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 bg-gray-900">
+        <Navbar />
+        <div className="p-6">
+          <EarningsReport />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Transactions;
