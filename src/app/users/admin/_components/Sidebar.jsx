35875cac19d123309920091df5a16cfff0e6e7a1
prow57naff <bed-com-08-19@unima.ac.mw>
2024-06-06 16:01:12 +0200

    Updated Sideba for routing pages

--- src/app/users/admin/_components/Sidebar.jsx
+++ src/app/users/admin/_components/Sidebar.jsx
@@ -1,30 +1,20 @@
-"use client"
+"use client";
 import { FaVideo, FaUser, FaCog, FaChartLine } from 'react-icons/fa';
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import Link from "next/link";
 
-
 const Sidebar = () => {
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      // Redirect to login page after logout
+      window.location.href = "/auth/signin";
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-    // const [username, setUsername] = useState(null);
-    // const [dropdownOpen, setDropdownOpen] = useState(false);
-  
-    // useEffect(() => {
-    //   getUserDetails();
-    // }, []);
-  
-    const logout = async () => {
-      try {
-        await axios.get("/api/users/logout");
-        toast.success("Logout successful");
-        // Redirect to login page after logout
-        window.location.href = "/auth/signin";
-      } catch (error) {
-        console.error(error.message);
-        toast.error("Failed to logout");
-      }
-    };
   return (
     <div className="bg-gray-900 text-white h-screen p-4">
       <div className="text-center my-6">
@@ -32,19 +22,44 @@ const Sidebar = () => {
       </div>
       <ul>
         <li className="my-4 flex items-center">
-          <FaVideo className="mr-2" /> <span>Dashboard</span>
+          <FaVideo className="mr-2" />
+          <Link href="/users/admin/dashboard">
+            <span>Dashboard</span>
+          </Link>
         </li>
         <li className="my-4 flex items-center">
-          <FaUser className="mr-2" /> <span>Users</span>
+          <FaUser className="mr-2" />
+          <Link href="/users/admin/users">
+            <span>Users</span>
+          </Link>
         </li>
         <li className="my-4 flex items-center">
-          <FaCog className="mr-2" /> <span>Settings</span>
+          <FaVideo className="mr-2" />
+          <Link href="/users/admin/videos">
+            <span>Videos</span>
+          </Link>
         </li>
         <li className="my-4 flex items-center">
-          <FaChartLine className="mr-2" /> <span>Reports</span>
+          <FaChartLine className="mr-2" />
+          <Link href="/users/admin/transactions">
+            <span>Transactions</span>
+          </Link>
         </li>
-        <a onClick={logout} className="block px-4 py-2 text-sm text-white bg-red-500 hover:bg-red-800 focus:outline-none">Logout</a>
-      
+        <li className="my-4 flex items-center">
+          <FaCog className="mr-2" />
+          <Link href="/users/admin/settings">
+            <span>Settings</span>
+          </Link>
+        </li>
+        <li className="my-4 flex items-center">
+          <FaChartLine className="mr-2" />
+          <Link href="/users/admin/reports">
+            <span>Reports</span>
+          </Link>
+        </li>
+        <a onClick={logout} className="block px-4 py-2 text-sm text-white bg-red-500 hover:bg-red-800 focus:outline-none cursor-pointer">
+          Logout
+        </a>
       </ul>
     </div>
   );
