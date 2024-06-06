38a8bf7ba2d592fc3f9bd4d062420bcb441f33a0
prow57naff <bed-com-08-19@unima.ac.mw>
2024-06-06 04:10:32 +0200

    Create search.jsx

---
+++ src/app/users/admin/_components/dashboard/search/search.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { MdSearch } from "react-icons/md";
+import styles from "./search.module.css";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+// import { useDebouncedCallback } from "use-debounce";
+
+const Search = ({ placeholder }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+ 
+
+  return (
+    <div className={styles.container}>
+      <MdSearch />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
+    </div>
+  );
+};
+
+export default Search;
