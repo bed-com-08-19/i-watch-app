af3a334864d4fc779743f5752119f6e96b77d8d3
Emmanuel Bester <96173167+bed-com-08-19@users.noreply.github.com>
2024-06-07 23:58:12 +0200

    Update videoModel.ts

--- src/models/videoModel.ts
+++ src/models/videoModel.ts
@@ -1,22 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const { Schema } = mongoose;
-
-// const videoSchema = new Schema({
-//   title: { type: String, required: true },
-//   description: { type: String, required: true },
-//   url: { type: String, required: true },
-//   category: { type: String },
-//   creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-//   views: { type: Number, default: 0 },
-//   earnings: { type: Number, default: 0 },
-//   createdAt: { type: Date, default: Date.now },
-// });
-
-// const Video = mongoose.model('Video', videoSchema);
-
-// export default Video;
-
 import mongoose, { Schema, Document } from "mongoose";
 
 interface IVideo extends Document {
