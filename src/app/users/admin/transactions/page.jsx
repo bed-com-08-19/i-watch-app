ddd68f321d8344b832ef987414852f880d548929
Emmanuel Bester <96173167+bed-com-08-19@users.noreply.github.com>
2024-06-11 01:21:59 +0200

    Update page.jsx

--- src/app/users/admin/transactions/page.jsx
+++ src/app/users/admin/transactions/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Sidebar from '../_components/Sidebar';
-import Navbar from '../_components/Navbar';
+import Header from '../_components/Header';
 import EarningsReport from '../_components/AdminTools/EarningsReport';
 
 const Transactions = () => {
@@ -8,7 +8,7 @@ const Transactions = () => {
     <div className="flex">
       <Sidebar />
       <div className="flex-1 bg-black-900">
-        <Navbar />
+        <Header />
         <div className="p-6">
           <EarningsReport />
         </div>
