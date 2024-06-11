a578c45907044d4773385832f3723f2374418502
Emmanuel Bester <96173167+bed-com-08-19@users.noreply.github.com>
2024-06-11 22:23:50 +0200

    Update page.tsx

--- src/app/users/creator/recommended/page.tsx
+++ src/app/users/creator/recommended/page.tsx
@@ -7,7 +7,7 @@ import Header from '../_components/Header';
 import Footer from '../../../../components/Footer';
 
 export default function UserProfile({ params }: any) {
-  const [categories, setCategories] = useState([]);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
     fetchVideos();
@@ -18,17 +18,8 @@ export default function UserProfile({ params }: any) {
       const res = await axios.get("/api/videos");
       const videos = res.data.data;
 
-      // if (videos.length === 0) {
-      //   setCategories([]);
-      // } else {
-      //   const newCategories = [
-      //     { name: "Newly Added", videos: videos.slice(0, 8) },
-      //     { name: "Mostly Liked", videos: videos.slice(8, 16) },
-      //   ];
-      //   setCategories(newCategories);
-      // }
+      setVideos(videos);
     } catch (error) {
-      
       toast.error("Failed to fetch videos");
     }
   };
@@ -37,27 +28,22 @@ export default function UserProfile({ params }: any) {
     <div>
       <Header />
       <main className="p-4">
-        {categories.length === 0 ? (
+        {videos.length === 0 ? (
           <div className="text-center text-white">
             <p>No videos available</p>
           </div>
         ) : (
-          categories.map((category) => (
-            <div key={category.name} className="mb-8">
-              <h2 className="text-2xl font-bold text-red-800 mb-4">{category.name}</h2>
-              <div className="grid grid-cols-2 sm:flex overflow-x-auto space-x-4">
-                {category.videos.map((video) => (
-                  <div key={video._id} className="relative h-48 sm:h-64">
-                    <video
-                      className="object-cover w-full h-full"
-                      src={video.url}
-                      controls
-                    />
-                  </div>
-                ))}
+          <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+            {videos.map((video) => (
+              <div key={video._id} className="relative h-48 sm:h-64">
+                <video
+                  className="object-cover w-full h-full"
+                  src={video.url}
+                  controls
+                />
               </div>
-            </div>
-          ))
+            ))}
+          </div>
         )}
       </main>
       <Footer />
