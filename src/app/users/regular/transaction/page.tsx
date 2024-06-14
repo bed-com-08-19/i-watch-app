057c635c8931ee5abba4b68a27427a330ce0d407
prow57naff <bed-com-08-19@unima.ac.mw>
2024-06-14 05:50:01 +0200

    Update page.tsx

--- src/app/users/regular/transaction/page.tsx
+++ src/app/users/regular/transaction/page.tsx
@@ -4,9 +4,28 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation'; // Correct import
 import { FiHome } from "react-icons/fi";
 import { BiArrowBack } from "react-icons/bi";
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 function Page() {
-  const [balance, setBalance] = useState(1000); // Initial balance
+
+  const [balance, setBalance] = useState("null");
+
+  seEffect(() => {
+    getUserDetails();
+  }, []);
+
+  const getUserDetails = async () => {
+    try {
+      const res = await axios.get("/api/users/me");
+      setBalance(res.data.data.balance);
+      
+    } catch (error) {
+      
+      toast.error("Failed to fetch user details");
+    }
+  };
+
   const router = useRouter();
 
   const handleWithdraw = () => {
@@ -22,7 +41,7 @@ function Page() {
       <div className="bg-black bg-opacity-70 px-10 py-16 rounded-md w-full max-w-md">
         <h1 className="text-4xl mb-8 font-semibold text-center">Your Balance</h1>
         <p className="font-bold text-3xl text-center px-4 py-2 rounded-md bg-gray-800 border border-gray-700 focus:outline-none focus:border-red-500">
-          K{balance.toFixed(2)}
+          K{balance}
         </p>
         <button
           className="mt-4 px-4 py-2 text-white bg-red-500 hover:bg-red-600 rounded-lg w-full"
@@ -36,3 +55,7 @@ function Page() {
 }
 
 export default Page;
+function seEffect(arg0: () => void, arg1: never[]) {
+  throw new Error('Function not implemented.');
+}
+
