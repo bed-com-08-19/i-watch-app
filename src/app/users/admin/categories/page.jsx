dd9ac934a6fc09cfabbfcd040650aaaefa95da56
prow57naff <bed-com-08-19@unima.ac.mw>
2024-06-17 17:57:23 +0200

    Update page.jsx

--- src/app/users/admin/categories/page.jsx
+++ src/app/users/admin/categories/page.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 import { FaTrash, FaEdit } from "react-icons/fa";
-import Sidebar from "./Sidebar";
+import Sidebar from "./_components/Sidebar";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
