2feda0b89cb04e05c04af472ddc98aa1b9f9dcf7
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-06-20 00:15:02 +0300

    Menu items for creator sidebar

---
+++ src/app/menu_constants.tsx
@@ -0,0 +1,51 @@
+import { SideNavItemGroup } from "../../types/type";
+import { BsEnvelope, BsGear, BsHouseDoor, BsKanban, BsListUl, BsQuestionCircle } from "react-icons/bs";
+import { FaUser, FaCog, FaChartLine, FaMoneyBillWave, FaHome, FaQuestionCircle, FaCoins } from 'react-icons/fa';
+
+
+
+export const SIDENAV_ITEMS: SideNavItemGroup[] = [
+
+    {
+        title: "Dashboards",
+        menuList: [
+            {
+                title: 'Home',
+                path: '/users/creator',
+                icon: <FaHome className="mr-2" size={20} />,
+            },
+            {
+                title: 'Profile',
+                path: '/users/creator//profile',
+                icon: <FaUser className="mr-2" size={20}/>,
+            },
+            {
+                title: 'Subscribe',
+                path: '/users/creator/subscribe',
+                icon: <FaMoneyBillWave className="mr-2" size={20}/>,
+            },
+            {
+                title: 'Withdraw',
+                path: '/users/creator/transaction',
+                icon: <FaChartLine className="mr-2" size={20}/>,
+            },
+            {
+                title: 'Top Up Coins',
+                path: '/users/creator/topup',
+                icon: <FaCoins className="mr-2" size={20}/>,
+            },
+            {
+                title: 'Settings',
+                path: '/users/creator/settings',
+                icon: <FaCog className="mr-2" size={20}/>,
+            },
+            {
+                title: 'Help',
+                path: '/users/creator/help',
+                icon: <FaQuestionCircle className="mr-2" size={20}/>,
+            }
+
+    ]
+}
+
+]
