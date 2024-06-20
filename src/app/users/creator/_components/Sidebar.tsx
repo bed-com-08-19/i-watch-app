9320535b39ed7da1fe2f9f9b31a57ade4d9a1172
bed-com-07-19 <bed-com-07-19@unima.ac.mw>
2024-06-20 16:27:59 +0300

    logout icon view when sidebar collapsed

--- src/app/users/creator/_components/Sidebar.tsx
+++ src/app/users/creator/_components/Sidebar.tsx
@@ -77,7 +77,6 @@
 
 // export default CreatorSidebar;
 
-
 'use client'
 import axios from "axios";
 import { SIDENAV_ITEMS } from '@/app/menu_constants';
@@ -85,53 +84,51 @@ import classNames from 'classnames';
 import React, { useEffect, useState } from 'react'
 import { useSideBarToggle } from '../../hooks/use-sidebar-toggle';
 import SideBarMenuGroup from './sidebar-menu-group';
-import { FaUser, FaCog, FaChartLine, FaMoneyBillWave, FaHome, FaQuestionCircle, FaCoins, FaSignOutAlt } from 'react-icons/fa';
-// import { SideBarLogo } from '../../../../../components/sidebar-logo';
-
+import { FaSignOutAlt } from 'react-icons/fa';
 
 export const SideBar = () => {
     const [mounted, setMounted] = useState(false);
     const { toggleCollapse } = useSideBarToggle();
 
-    const asideStyle = classNames("sidebar overflow-y-auto overflow-x-auto fixed bg-gray-900 h-full shadow-sm shadow-slate-500/40 transition duration-300 ease-in-out z-[99999]",
-        {
-            ["w-[20rem]"]: !toggleCollapse,
-            ["sm:w-[5.4rem] sm:left-0 left-[-100%]"]: toggleCollapse,
-        });
+    const asideStyle = classNames("sidebar flex flex-col justify-between overflow-y-auto fixed bg-gray-900 h-full shadow-sm shadow-slate-500/40 transition duration-300 ease-in-out z-[99999]", {
+        ["w-[20rem]"]: !toggleCollapse,
+        ["sm:w-[5.4rem] sm:left-0 left-[-100%]"]: toggleCollapse,
+    });
 
-  const logout = async () => {
-    try {
-      await axios.get("/api/users/logout");
-      window.location.href = "/auth/signin";
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            window.location.href = "/auth/signin";
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
 
     useEffect(() => setMounted(true), []);
 
     return (
         <aside className={asideStyle}>
-            <div className="sidebar-top relative flex items-center px-3.5 py-5">
-                {mounted}
-                <h3 className={classNames("pl-2 font-bold text-2xl min-w-max text-sidebar-foreground",
-                    { hidden: toggleCollapse })}>
-                    Creator Profile</h3>
-            </div>
-            <nav className="flex flex-col gap-2 transition duration-300 ease-in-out">
-                <div className="flex flex-col gap-2 px-4">
-                    {SIDENAV_ITEMS.map((item, idx) => {
-                        return <SideBarMenuGroup key={idx} menuGroup={item} />;
-                    })}
+            <div>
+                <div className="sidebar-top relative flex items-center px-3.5 py-5">
+                    {mounted && (
+                        <h3 className={classNames("pl-2 font-bold text-2xl min-w-max text-sidebar-foreground", { hidden: toggleCollapse })}>
+                            Creator Profile
+                        </h3>
+                    )}
                 </div>
-                <button
-                    onClick={logout}
-                    className="flex items-center justify-center rounded m-4 px-4 py-2 text-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none cursor-pointer"
-                >
-                    <FaSignOutAlt className="mr-2" size={20}/> <span>Logout</span>
-                </button>
-            </nav>
+                <nav className="flex flex-col gap-2 px-4">
+                    {SIDENAV_ITEMS.map((item, idx) => (
+                        <SideBarMenuGroup key={idx} menuGroup={item} />
+                    ))}
+                </nav>
+            </div>
+            <button
+                onClick={logout}
+                className="flex items-center justify-center rounded m-4 px-4 py-2 text-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none cursor-pointer"
+            >
+                <FaSignOutAlt className="mr-2" size={20} />
+                {!toggleCollapse && <span>Logout</span>}
+            </button>
         </aside>
-    )
+    );
 }
-
